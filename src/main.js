4f533eeb1735c07c9cfb23340c3fa26b7a74968f
Galez <gustheworld6@gmail.com>
2023-03-17 17:36:42 +0700

    Update full searchBar
    success to create search bar

--- src/main.js
+++ src/main.js
@@ -25,5 +25,15 @@ const changeBg = () =>{
   }
 }
 
+const toggleSearch = (search,button) => {
+  const searchBar = document.getElementById(search),
+        searchButton = document.getElementById(button)
+
+  searchButton.addEventListener('click', () => {
+    searchBar.classList.toggle('show-search')
+  })
+}
+
+toggleSearch('search-bar','search-button')
 window.addEventListener('scroll',changeBg)
 
